9a56952def6034c256a6c22111745db64c6663de
Sebastian Otto <sotto@wemakeweb.de>
2014-08-16 02:14:37 +0200

    added better react js example

--- app/public/assets/js/react.js
+++ app/public/assets/js/react.js
@@ -1,15 +1,22 @@
-/** @jsx React.DOM */
-var React = require('react');
+/**
+ * @jsx React.DOM
+ */
 
-var Hello = React.createClass({
-  render: function() {
-    return <div>Hello, {this.props.name}!</div>
-  }
-})
+var React = require('React');
+var ExampleApplication = React.createClass({
+render: function() {
+  var elapsed = Math.round(this.props.elapsed  / 100);
+  var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+  var message =
+    'React has been successfully running for ' + seconds + ' seconds.';
 
+  return <p>{message}</p>;
+}
+});
+var start = new Date().getTime();
+setInterval(function() {
 React.renderComponent(
-  <Hello name="World" />,
+  <ExampleApplication elapsed={new Date().getTime() - start} />,
   document.getElementById('hello')
-)
-
-module.exports = 1;
+);
+}, 50);
